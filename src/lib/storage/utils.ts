3a360c7b0cd0c137ec75e31b4a36f054192ebd22
Jose Manuel Veliz <ducen29@gmail.com>
2025-11-07 17:49:52 -0400

    feat(storage): implement file upload functionality with Uploadthing integration

---
+++ src/lib/storage/utils.ts
@@ -0,0 +1,7 @@
+import { generateReactHelpers } from "@uploadthing/react";
+
+import type { OurFileRouter } from "./core";
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
+
